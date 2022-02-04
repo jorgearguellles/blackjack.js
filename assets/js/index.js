d46b407e71cc82345d74d0cf62f98c0a05a7b3d7
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-02-03 20:57:13 -0500

    add/ imgs carts, DOM manioulation adn logic player one done and part of the CPU logic

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,5 +2,105 @@
 2C => two clubs 
 2D => two Diamonds
 2H => two Hearts 
-2S => two Swords 
+2S => two Spades
 */
+
+let deck = [];
+const types = ['C','D','H','S'];
+const capitals = ['A','J','Q','K'];
+let scorePlayer1 = 0;
+let scorePlayer2 = 0;
+
+//Refs HTML
+const btnNew = document.querySelector('#btnNew');
+const btnCall = document.querySelector('#btnCall');
+const btnStop = document.querySelector('#btnStop');
+const scoreList = document.querySelectorAll('small');
+const containerCartsP1 = document.querySelector('#playerOne');
+const containerCartsP2 = document.querySelector('#playerTwo');
+
+
+
+const createDeck = () => {
+  
+  for( let i = 2; i <= 10; i++ ){
+    for (let type of types){
+      deck.push( i + type )
+    }
+  }
+
+  for( let cap of capitals){
+    for (let type of types){
+      deck.push( cap + type)
+    }
+  }
+
+  return _.shuffle(deck);
+};
+deck = createDeck();
+
+// Function to take a card
+
+const callACart = ( ) => {
+  if (deck.length === 0) throw 'Deck empty';
+  return deck.shift();
+};
+
+
+const cartValue = ( cart ) => {
+  const value = cart.substring(0, cart.length - 1)
+  return (!isNaN(value)) ? value * 1 : (value === 'A') ? 11 : 10
+};
+
+// Player 2 logic
+
+const player2Turn = ( minScore) => {
+  
+  do {
+    const cart = callACart();
+    scorePlayer2 = scorePlayer2 + cartValue(cart);
+
+    // Show points
+    scoreList[1].innerText = scorePlayer2;
+
+    // Show carts in table troughs containerCartsP1
+    const imgCart = document.createElement('img');
+    imgCart.src = `./assets/carts/${cart}.png`; // dynamic src
+    imgCart.classList.add('cart') // cart style class applied
+    containerCartsP2.append(imgCart) // append the new cart into container carts Plater One
+
+    if(minScore > 21){
+      break
+    };
+
+  } while( (scorePlayer2 < minScore) && (minScore <= 21 ) );
+
+}
+
+
+// Events
+
+btnCall.addEventListener('click', ()=>{
+  const cart = callACart();
+  scorePlayer1 = scorePlayer1 + cartValue(cart);
+
+  // Show points
+  scoreList[0].innerText = scorePlayer1;
+
+  // Show carts in table troughs containerCartsP1
+  const imgCart = document.createElement('img');
+  imgCart.src = `./assets/carts/${cart}.png`; // dynamic src
+  imgCart.classList.add('cart') // cart style class applied
+  containerCartsP1.append(imgCart) // append the new cart into container carts Plater One
+
+  //Handle points to lose, win 
+  if( scorePlayer1 > 21){
+    console.warn("Lose")
+    btnCall.disabled = true;
+    player2Turn(scorePlayer1);
+
+  } else if( scorePlayer1 === 21) {
+    console.warn("21, you win!")
+    btnCall.disabled = true;
+  }
+});
