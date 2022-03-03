8dee5bec48fab93b3ac1c3d3b12671d6e925b21c
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-03-02 22:28:22 -0500

    add/ second part optimization done

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,19 +2,29 @@
 
 (()=>{
   'use strict'
-  let deck = [];
+
+  let deck = [],
+      playersPoints = []; // [player1, player2, playerN = CPU]
+
   const types = ['C','D','H','S'],
         capitals = ['A','J','Q','K'];
-  let scorePlayer1 = 0;
-  let scorePlayer2 = 0;
-
+  
   //Refs HTML
   const btnNew = document.querySelector('#btnNew'),
         btnCall = document.querySelector('#btnCall'),
         btnStop = document.querySelector('#btnStop');
+
   const scoreList = document.querySelectorAll('small'),
-        containerCartsP1 = document.querySelector('#playerOne'),
-        containerCartsP2 = document.querySelector('#playerTwo');
+        divPlayersCarts = document.querySelectorAll('.divCarts');
+
+
+  // Initialized Game
+  const initializedGame = ( numPlayers = 2 ) => {
+    deck = createDeck();
+    for( let i = 0; i < numPlayers; i++ ){
+      playersPoints.push(0);
+    };
+  };
 
   //Function to create Deck
   const createDeck = () => {
@@ -32,47 +42,54 @@
     return _.shuffle(deck);
   };
 
-  // Function to take a card
+  // Function to call card
   const callACart = ( ) => {
-    deck = createDeck();
-
     if (deck.length === 0) throw 'Deck empty';
-    return deck.shift();
+    return deck.pop();
   };
 
+  // Function to get card value
   const cartValue = ( cart ) => {
     const value = cart.substring(0, cart.length - 1)
     return (!isNaN(value)) ? value * 1 : (value === 'A') ? 11 : 10
   };
 
-  // Player 2 logic
-  const player2Turn = ( minScore) => {
-    
+  // Player 2 logic (CPU)
+  // turn: 0 = Player1, 1=player2, ..., playerN = CPU
+  const accumulatePoints = ( cart, turn ) => {
+    playersPoints[turn] = playersPoints[turn] + cartValue( cart );
+    scoreList[turn].innerText = playersPoints[turn];
+    return playersPoints[turn];
+  };
+
+  const createCart = ( cart, turn ) => {
+    const imgCart = document.createElement('img');
+    imgCart.src = `./assets/carts/${cart}.png`; 
+    imgCart.classList.add('cart')
+    divPlayersCarts[turn].append(imgCart);
+  };
+
+  const cpuTurn = ( minScore ) => {
+
+    let scoreCPU = 0;
+
     do {
       const cart = callACart();
-      scorePlayer2 = scorePlayer2 + cartValue(cart);
-
-      // Show points
-      scoreList[1].innerText = scorePlayer2;
-
-      // Show carts in table troughs containerCartsP1
-      const imgCart = document.createElement('img');
-      imgCart.src = `./assets/carts/${cart}.png`; // dynamic src
-      imgCart.classList.add('cart') // cart style class applied
-      containerCartsP2.append(imgCart) // append the new cart into container carts Plater One
-
+      scoreCPU = accumulatePoints(cart, playersPoints.length - 1)
+      createCart( cart, playersPoints.length - 1 );
+      
       if(minScore > 21){
         break
       };
 
-    } while( (scorePlayer2 < minScore) && (minScore <= 21 ) );
+    } while( (scoreCPU < minScore) && (minScore <= 21 ) );
 
     setTimeout( ()=>{
-      if(scorePlayer2 === minScore){
+      if(scoreCPU === minScore){
         alert(`Both player have the same Score ${minScore}`)
       }else if (minScore > 21 ){
         alert(`CPU win!`)
-      } else if( scorePlayer2 > 21){
+      } else if( scoreCPU > 21){
         alert(`Congratulations you win!`)
       } else {
         alert('CPU win!')
@@ -83,23 +100,17 @@
 
   // Events
   btnCall.addEventListener('click', ()=>{
+
     const cart = callACart();
-    scorePlayer1 = scorePlayer1 + cartValue(cart);
-
-    // Show points
-    scoreList[0].innerText = scorePlayer1;
-
-    // Show carts in table troughs containerCartsP1
-    const imgCart = document.createElement('img');
-    imgCart.src = `./assets/carts/${cart}.png`; // dynamic src
-    imgCart.classList.add('cart') // cart style class applied
-    containerCartsP1.append(imgCart) // append the new cart into container carts Plater One
+    const scorePlayer1 =  accumulatePoints( cart, 0);
+    createCart( cart, 0);
+  
 
     //Handle points to lose, win 
     if( scorePlayer1 > 21){
       btnCall.disabled = true;
       btnStop.disabled = true;
-      player2Turn(scorePlayer1);
+      cpuTurn(scorePlayer1);
 
     } else if( scorePlayer1 === 21) {
       btnCall.disabled = true;
@@ -109,22 +120,29 @@
   });
 
   btnStop.addEventListener('click', ()=>{
+
     btnCall.disabled = true;
     btnStop.disabled = true;
-    player2Turn(scorePlayer1)
+    cpuTurn(scorePlayer1)
   })
 
   btnNew.addEventListener('click', ()=>{
-    deck = [];
-    deck = createDeck();
-    scorePlayer1 = 0;
-    scorePlayer2 = 0;
-    scoreList[0].innerText = 0;
-    scoreList[1].innerText = 0;
-    containerCartsP1.innerHTML = '';
-    containerCartsP2.innerHTML = '';
-    btnCall.disabled = false;
-    btnStop.disabled = false;
+
+    console.clear();
+    initializedGame();
+    // deck = [];
+    // deck = createDeck();
+    // scorePlayer1 = 0;
+    // scoreCPU = 0;
+
+    // scoreList[0].innerText = 0;
+    // scoreList[1].innerText = 0;
+
+    // containerCartsP1.innerHTML = '';
+    // containerCartsP2.innerHTML = '';
+
+    // btnCall.disabled = false;
+    // btnStop.disabled = false;
   })
 
 })()
